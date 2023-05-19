b35106f5ad912c3e7837d09beb6110312b34b276
mrtoxa42 <betebet16@hotmail.com>
2023-05-19 20:03:07 +0300

    Karakter
    Karakterin hareket kodlarını halletim.

--- Scripts/Menu/StartScreen.gd
+++ Scripts/Menu/StartScreen.gd
@@ -5,7 +5,7 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 #	if anim_name == "EntertaimentAnimation":
 #		$AnimationPlayer.play("PresentAnimation")
 	if anim_name == "PresentAnimation":
-		pass
+		print("Merhaba alper")
 
 
 
