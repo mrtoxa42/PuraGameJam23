9f7ac3f6e2057dfc194f2676e27eae7360e9f871
mrtoxa42 <betebet16@hotmail.com>
2023-05-20 12:55:13 +0300

    Karakter GÜncelleme
    Tarığın çizdiği karakteri animasyonları ile birlikte oyuna ekledim.

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -12,9 +12,17 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = 1
+		rotation_degrees = 45
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -1
-
+		rotation_degrees = -45
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = 1
+	if velocity.x == 0:
+		rotation_degrees = 0
+		
 	if activejump == false:
 		if Input.is_action_just_pressed("ui_select"):
 			activejump = true
