7d68b434067ad8df0ef49879d8a9024a44db2c2c
Emre <emreee.dindar@gmail.com>
2023-05-20 18:22:42 +0300

    Tutorial
    Tutorial oldu

--- Scripts/Menu/GameOver.gd
+++ Scripts/Menu/GameOver.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var Go = "Menu"
+
 
 func _ready():
 	GameManager.gamein = false
@@ -8,3 +10,16 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_select"):
 		get_tree().change_scene("res://Scenes/Game/MainGame.tscn")
+
+
+func _on_MenuButton_pressed():
+	$Timer.start()
+	Go = "Menu"
+	
+	$Animation.play("BalonAni")
+	
+
+
+func _on_Timer_timeout():
+	if Go == "Menu" :
+		get_tree().change_scene("res://Scenes/Menu/Menu.tscn")
